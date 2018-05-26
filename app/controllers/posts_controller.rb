551f547b1ab016454edafdecbc67b24761ac6178
Kevin <kyrodriguez97@gmail.com>
2018-05-26 17:28:44 -0500

    Posts model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def posts_params
+
+  end
+end
