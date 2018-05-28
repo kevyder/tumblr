c00a5745dcc3c001efe11fa2c1487c8920b548d6
Kevin <kyrodriguez97@gmail.com>
2018-05-27 20:19:45 -0500

    Add auth and user restriction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_post, only: [:show, :update, :destroy, :edit]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
